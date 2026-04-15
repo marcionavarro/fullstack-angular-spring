c3b901b1597a6afeac5b16e4022bb681e7e48483
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-04-15 20:28:57 -0300

    5. Relacionamento entre recursos REST - 5.3-desafio-cadastrando-o-primeiro-lancamento, 5.4-validando-incosistencias, 5.5-validando-lancamento-com-bean-validation, 5.6-regra-para-nao-salvar-pessoa-inativa

--- algamoney-api/src/main/java/com/example/algamoney/api/model/Lancamento.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/model/Lancamento.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "lancamento")
@@ -22,25 +23,32 @@ public class Lancamento {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotNull
 	private String descricao;
 
+	@NotNull
 	@Column(name = "data_vencimento")
 	private LocalDate dataVencimento;
 
 	@Column(name = "data_pagamento")
 	private LocalDate dataPagamento;
 
+	@NotNull
 	private BigDecimal valor;
 
 	private String observacao;
 	
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private TipoLancamento tipo;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
 	private Categoria categoria;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
 	private Pessoa pessoa;
--- algamoney-api/src/main/java/com/example/algamoney/api/model/Pessoa.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -2,12 +2,15 @@ package com.example.algamoney.api.model;
 
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
@@ -59,6 +62,12 @@ public class Pessoa {
 		this.ativo = ativo;
 	}
 
+	@JsonIgnore
+	@Transient
+	public boolean isInativo() {
+		return !this.ativo;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(codigo);
