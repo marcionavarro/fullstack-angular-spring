dd4ce467bc25853641f31ab24e43552885f86e6b
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-09-19 11:04:24 -0300

    9. Fundamentos do Angular, componentes e data biding - FAS_9.1-bootstrapping-e-appmodule, FAS_9.2-o-que-sao-componentes, FAS_9.3-criando-um-componente

--- meuprojeto/src/app/app.ts
+++ meuprojeto/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Hello } from './hello/hello';
+import { BemVindo } from './bem-vindo/bem-vindo';
 
 @Component({
-  imports: [],
+  imports: [Hello, BemVindo],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
---
+++ meuprojeto/src/app/bem-vindo/bem-vindo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-bem-vindo',
+  styleUrl: './bem-vindo.css',
+  templateUrl: './bem-vindo.html',
+})
+export class BemVindo {}
